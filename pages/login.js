6704cf85f1f7ef01fe0ed9be0723f49956ce3f82
wo3214 <wo32144@naver.com>
2023-03-17 01:34:55 +0900

    0317

--- pages/login.js
+++ pages/login.js
@@ -1,26 +1,46 @@
 import { signIn } from "next-auth/react";
 import style from "@/styles/login/login.module.scss";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function LoginPage() {
   return (
     <>
       <div className={style.loginBack}>
         <div className={style.loginBox}>
-          <Image
-            src="/assets/images/logo.svg"
-            alt="로고"
-            width={150}
-            height={150}
-          />
-          <div
-            className={style.googleBtn}
+          <motion.div
+            className={style.loginAni}
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.2,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+          >
+            <Image
+              src="/assets/images/logo.svg"
+              alt="로고"
+              width={150}
+              height={150}
+            />
+          </motion.div>
+
+          <motion.div
             onClick={() => {
               signIn("google", { callbackUrl: "http://localhost:3000" });
             }}
+            className={style.googleBtn}
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{
+              duration: 0.7,
+              delay: 0.4,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
           >
             <img src="/assets/images/login/btn_google_signin_light_normal_web@2x.png" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </>
--- pages/mission/missionModal.js
+++ pages/mission/missionModal.js
@@ -1,11 +1,6 @@
-
-
+import React from "react";
 import style from "@/styles/mission/mission.module.css";
 import { useRouter } from "next/router";
-
-import React , {useState, useEffect} from'react'
-
-
 
 import Image from "next/image";
 import MissionCatch from "@/components/mission/MissionCatch";
@@ -15,20 +10,19 @@ import MissonMotionBox from "@/components/mission/MissonMotionBox";
 import MissionClickCount from "@/components/mission/MissionClickCount";
 import MissionTypeWithKeyBoard from "@/components/mission/MissionTypeWithKeyBoard";
 import MissionCoinBox from "@/components/mission/MissionCoinBox";
-import MissionJackPot from "@/components/mission/MissionJackPot";
 
-//list에 컴포넌트 타입 넣기 
+//list에 컴포넌트 타입 넣기
 const componentList = [
-    MissonMotionBox,
-    MissionClickCount,
-    MissionTypeWithKeyBoard,
-    MissionTree,
-    MissionDiary,
-    MissionCatch
-  ];
+  MissonMotionBox,
+  MissionClickCount,
+  MissionTypeWithKeyBoard,
+  MissionTree,
+  MissionDiary,
+  MissionCatch,
+  MissionCoinBox,
+];
 
-
-  // 랜덤한 컴포넌트를 생성하여 반환    
+// 랜덤한 컴포넌트를 생성하여 반환
 const getRandomComponent = () => {
   const randomIndex = Math.floor(Math.random() * componentList.length);
   const RandomComponent = componentList[randomIndex];
@@ -36,23 +30,15 @@ const getRandomComponent = () => {
 };
 
 const Mission = () => {
-
-      const router = useRouter();
-  
+  const router = useRouter();
 
   return (
     <>
       <div className={style.modalWrapper}>
-        <div className={style.missionModal}>
-          {getRandomComponent()}
-        </div>
+        <div className={style.missionModal}>{getRandomComponent()}</div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Mission
-
-
-
-
+export default Mission;
