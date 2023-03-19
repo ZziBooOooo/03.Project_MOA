da01e3b6f08633fd75427d6cfc1ce9a5da389708
wo3214 <wo32144@naver.com>
2023-03-20 01:39:00 +0900

    0320

--- components/Footer.js
+++ components/Footer.js
@@ -3,20 +3,25 @@ import style from "../styles/common.module.css";
 
 const Footer = () => {
   return (
-      <footer className={style.footerBox}>
-        <div className={style.footerContainer}>
-          <p className={style.footer_title}>고객센터</p>
-          <div className={style.footer_detail}>
-            <p>
+    <footer className={style.footerBox}>
+      <div className={style.footerContainer}>
+        <p className={style.footer_title}>고객센터</p>
+        <div className={style.footer_detail}>
+          <p>
             전화: 1004-1004<br></br>
-            이메일: <a href="#">moamoa@moa.com</a><br></br>
-            깃허브 주소: <a href="https://github.com/Green-Team-D/MOA" target='blank'>https://github.com/Green-Team-D/MOA</a><br></br>
+            이메일: <a href="#">moamoa@moa.com</a>
+            <br></br>
+            깃허브 주소:{" "}
+            <a href="https://github.com/Green-Team-D/MOA" target="blank">
+              https://github.com/Green-Team-D/MOA
+            </a>
+            <br></br>
             대표: 조민서
-            </p>    
-          </div>
+          </p>
         </div>
-      </footer>
-  )
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Header.js
+++ components/Header.js
@@ -168,8 +168,52 @@ const Header = () => {
               </div>
             </>
           ) : (
-            <p onClick={() => router.push("/login")}>로그인</p>
+            <p
+              onClick={() => router.push("/login")}
+              className={style.mobileShow}
+            >
+              로그인
+            </p>
           )}
+        </div>
+      </div>
+      <div className={style.mobileMenu}>
+        <div>
+          <p
+            onClick={() => {
+              router.push("/mission");
+            }}
+          >
+            미션하기
+          </p>
+          <p
+            onClick={() => {
+              router.push("/buypage");
+            }}
+          >
+            구매하기
+          </p>
+          <p
+            onClick={() => {
+              router.push("/generate");
+            }}
+          >
+            만들기
+          </p>
+          <p
+            onClick={() => {
+              router.push("/myalbum");
+            }}
+          >
+            앨범
+          </p>
+          <p
+            onClick={() => {
+              router.push("/generate/others");
+            }}
+          >
+            좋아요
+          </p>
         </div>
       </div>
     </div>
